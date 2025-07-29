cb26ff750ba618bf619ec46da95ae036d41605d7
Michael Rosendal <65503882+mbarosendal@users.noreply.github.com>
2025-07-29 14:04:52 +0200

    Enhance modals, timers, and perform code cleanup
    - Added `OnClose` callback to `BaseModal` and `SettingsModal` for handling close events.
    - Updated `Index.razor` to reset timers and refresh UI on modal close.
    - Refactored and reorganized methods in `Index.razor` for clarity.
    - Modified `TimerService` to return "Due" instead of "Overdue" for past times.
    - Cleaned up and reformatted CSS styles for consistency.
    - Performed general code cleanup: removed unused code, aligned attributes, and improved readability.

--- TickTask/Client/Services/TimerService.cs
+++ TickTask/Client/Services/TimerService.cs
@@ -84,7 +84,7 @@ namespace TickTask.Client.Services
 
             var estimatedTime = DateTime.UtcNow + TimeSpan.FromMinutes(remainingCurrentPomodoroTime + remainingPomodorosTime + shortBreaksTime + longBreaksTime);
 
-            return estimatedTime < DateTime.UtcNow ? "Overdue" : estimatedTime.ToLocalTime().ToString("HH:mm");
+            return estimatedTime < DateTime.UtcNow ? "Due" : estimatedTime.ToLocalTime().ToString("HH:mm");
         }
 
     }
